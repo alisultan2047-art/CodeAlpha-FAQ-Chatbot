0c79614d9a84f1bd56a64c161df89329255f7f06
Ali Sultan <alisultan2047@gmail.com>
2026-09-08 21:53:51 +0500

    Update API endpoint to production URL

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
     setInput("");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/chat", {
+       const response = await fetch("https://codealpha-faq-chatbot-5ug4.onrender.com/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: userQuery })
